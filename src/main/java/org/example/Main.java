5a636abb0fd7044305c6f86f0902b1c42f2d4bc2
MaggySanchez <maguitta20@gmail.com>
2023-11-06 15:59:07 -0300

    Maven, dependecies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Main {
+    static Logger log;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log = LogManager.getLogger("prueba");
     }
 }
--- src/main/java/org/example/clases/Leon.java
+++ src/main/java/org/example/clases/Leon.java
@@ -1,4 +1,46 @@
 package org.example.clases;
 
 public class Leon {
+    private String nombre;
+    private int edad;
+    private Boolean esAlfa;
+
+    public Leon(String nombre, int edad, Boolean esAlfa) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.esAlfa = esAlfa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Boolean getEsAlfa() {
+        return esAlfa;
+    }
+
+    public void setEsAlfa(Boolean esAlfa) {
+        this.esAlfa = esAlfa;
+    }
+
+    @Override
+    public String toString() {
+        return "Leon{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", esAlfa=" + esAlfa +
+                '}';
+    }
 }
