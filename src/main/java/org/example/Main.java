7ced193a26126a255e4a10c05afc877d24858081
MaggySanchez <maguitta20@gmail.com>
2023-11-08 15:59:47 -0300

    JBDC y H2, creacion de table TEST_CLIENTE y carga de dos registros

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,7 @@
 package org.example;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.example.clases.Leon;
 
 public class Main {
-    static Logger log;
-    public static void main(String[] args) {
-        Leon leon = new Leon("Mufasa", 15, true);
-        leon.esAlfaMayor();
-        try {
-            leon.setEdad(-2);
-        } catch (IllegalArgumentException e) {
-            System.err.println("Ingreso una edad invalida.");
-        } finally {
-            System.out.println("Finalizo el proceso.");
-        }
-    }
+
 }
